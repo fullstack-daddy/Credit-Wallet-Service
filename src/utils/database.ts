bc78c18e474ecf5457dc8d0381d456dad85fd6f4
fullstack-daddy <aboyejiemmanuel07@gmail.com>
2024-07-18 14:27:59 +0100

    bug fix

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,8 +1,19 @@
 import Knex from 'knex';
-import knexConfig from '../../knexfile.js';
 
-const environment = process.env.NODE_ENV || 'development';
-const config = knexConfig[environment];
-const knex = Knex(config);
+const knex = Knex({
+  client: 'mysql2',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: parseInt(process.env.DB_PORT || '3306'),
+    ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : undefined,
+  },
+  pool: {
+    min: 2,
+    max: 10
+  }
+});
 
 export default knex;
