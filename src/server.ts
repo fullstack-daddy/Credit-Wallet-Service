5bb7902fd237677af10544acf62e1f2abe1d61f3
fullstack-daddy <aboyejiemmanuel07@gmail.com>
2024-07-19 13:32:53 +0100

    dependency bug fix

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import accountRoutes from './routes/accountRoutes';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
